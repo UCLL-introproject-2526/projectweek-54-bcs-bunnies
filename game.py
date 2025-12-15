4fc0b90be048216b1af2e32bde9aef8a34b57b91
Belade Molefe Kodasso <r1024429@ucll.be>
2025-12-15 14:57:03 +0100

    game  v1

--- game.py
+++ game.py
@@ -1,7 +1,22 @@
+import pygame
+import time
+import random
+
+#setting display
+WIDTH,HEIGHT= 1000,800
+WIN= pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('bunnies beta V 1.0')
+
+#defining gameplay loop
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type==
 
 
 
-class game:
-    
+
 
 

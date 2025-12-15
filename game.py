d28b7be3be03dc0a4d4884a8c99a39a1ce7591cb
Dorina Osbáth <osbath30@gmail.com>
2025-12-15 16:03:06 +0100

    image

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ WIDTH,HEIGHT= 1000,800
 WIN= pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('bunnies beta V 1.0')
 
-BG =pygame.transform.scale(pygame.image.load("bg.png"), (WIDTH, HEIGHT))
+BG =pygame.transform.scale(pygame.image.load("images/bg.png"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 40
 PLAYER_HIGHT = 60
